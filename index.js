0a2930953d6d8ad282eb2cc730ccbfb05df743b6
emangomaa <emangomaa149@gmail.com>
2025-10-31 11:17:20 +0200

    dockeriza express app

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 const port = 3000;
-console.log("hello from server side");
-
+console.log("hello from server side!");
+app.get("/", (req, res) => {
+  res.send("Hello from server side!");
+});
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}!`);
 });
