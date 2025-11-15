f9e82dbb3310978e6b6c71a153c69438b76481df
emangomaa <emangomaa149@gmail.com>
2025-11-15 07:48:16 +0200

    add user as stripe customer

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import express from "express";
+import paymentRoutes from "./src/routes/payment.routes.js";
+
+import connectDB from "./database/dbConnection.js";
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = 3001;
+connectDB();
 console.log("hello from server side!");
-app.get("/", (req, res) => {
-  res.send("Hello from server side!");
-});
+app.use("/api/v1/payment", paymentRoutes);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!`);
 });
